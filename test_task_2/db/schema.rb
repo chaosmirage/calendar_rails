065943669e073ebb62c0587986421db3d7eb52cc
Adel Khamatov <adelkhamatov@yandex.ru>
2015-11-23 17:57:40 +0300

    Created user model

--- test_task_2/db/schema.rb
+++ test_task_2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120110525) do
+ActiveRecord::Schema.define(version: 20151123145130) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20151120110525) do
     t.string   "type_repeat"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
