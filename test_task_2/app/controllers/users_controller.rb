6ad0bd6cf1a1f6f2ff9103e0bf9a27128c7283c9
Adel Khamatov <adelkhamatov@yandex.ru>
2015-11-26 17:30:55 +0300

    Created user model edit action

--- test_task_2/app/controllers/users_controller.rb
+++ test_task_2/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :find_item, only: [:show]
+  before_filter :find_user, only: [:show, :edit, :update]
 
   def show
 
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def edit
+
   end
 
   def create
@@ -20,13 +24,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user.update_attributes(user_params)
+    if @user.errors.empty?
+      flash[:success] = "Saved!"
+      redirect_to @user
+    else
+      flash.now[:danger] = "Didn't save!"
+      render 'new'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :name, :last_name, :password, :password_confirmation)
   end
 
-  def find_item
+  def find_user
     @user = User.where(id: params[:id]).first
 
     render_404 unless @user
