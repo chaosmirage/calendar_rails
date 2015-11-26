a68bbc030a4b54628acc03fcd05b7168e9ed1b62
Adel Khamatov <adelkhamatov@yandex.ru>
2015-11-26 16:55:06 +0300

    Add simple form; created user model action and view show, new

--- test_task_2/app/controllers/users_controller.rb
+++ test_task_2/app/controllers/users_controller.rb
@@ -6,15 +6,16 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to action: 'show'
+    @user = User.create(user_params)
+    if @user.errors.empty?
+      redirect_to user_path(@user)
     else
       render 'new'
+    end
   end
 
   private
