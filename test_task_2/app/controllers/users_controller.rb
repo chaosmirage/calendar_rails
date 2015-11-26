55bb11e33515a204845f1f40ba68901fa96c0857
Adel Khamatov <adelkhamatov@yandex.ru>
2015-11-26 17:20:43 +0300

    Add flash

--- test_task_2/app/controllers/users_controller.rb
+++ test_task_2/app/controllers/users_controller.rb
@@ -12,8 +12,10 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.errors.empty?
-      redirect_to user_path(@user)
+      flash[:success] = "Account created!"
+      redirect_to @user
     else
+      flash.now[:danger] = "Account didn't create!"
       render 'new'
     end
   end
